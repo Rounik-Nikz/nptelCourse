4f5cbfb5b758dd1e1f9b658a96cba406a9db4993
Rounik M. <71693970+Rounik-Nikz@users.noreply.github.com>
2025-01-07 10:29:13 +0530

    Update settings.py

--- PyChat/settings.py
+++ PyChat/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'usnm_pu9ng2xf4@q#aens^n4t)_1)b6icnc^1u-f_jko++*=(#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
 
